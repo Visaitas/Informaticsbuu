1d0aae97b6c8515ed4c2a0ebd50d1505f6d62486
Visaitas Somrith <36034869+Visaitas@users.noreply.github.com>
2019-01-24 15:53:39 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,8 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'NxmBKV8M7hITmg4GxPgMWwLy+3mnNzJ5eaYRLrN9HvRWfMbzs4Tiee3RcJNVQwr4+cwffA61U789x3PXIoUFAQsvGtxb2W4vLzTGHFxhaThUDOof2YjCIHREOn5qsnTs9EdiCd9qYQT93fl4HNMYwdB04t89/1O/w1cDnyilFU=
+'; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
