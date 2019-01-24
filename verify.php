71db7715e9163cab38b94dfd097edc750652caac
Visaitas Somrith <36034869+Visaitas@users.noreply.github.com>
2019-01-24 15:52:43 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,6 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = 'NxmBKV8M7hITmg4GxPgMWwLy+3mnNzJ5eaYRLrN9HvRWfMbzs4Tiee3RcJNVQwr4+cwffA61U789x3PXIoUFAQsvGtxb2W4vLzTGHFxhaThUDOof2YjCIHREOn5qsnTs9EdiCd9qYQT93fl4HNMYwdB04t89/1O/w1cDnyilFU=
+';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
